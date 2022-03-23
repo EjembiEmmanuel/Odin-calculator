9ad7bfd2de01028cdc6f2486c27e99b9109a869c
Ejembi Emmanuel <ejembioche50@gmail.com>
2022-03-23 22:10:15 +0100

    Created substract function

--- calculator.js
+++ calculator.js
@@ -4,4 +4,41 @@ function add (num1, num2) {
     return result;
 }
 
-console.log(add(5, 5));
+function substract (num1, num2) {
+    let result;
+    result = num1 - num2;
+    return result;
+}
+
+// function multiply (num1, num2) {
+//     let result;
+//     result = num1 * num2;
+//     return result;
+// }
+
+// function divide (num1, num2) {
+//     let result;
+//     result = num1 / num2;
+//     return result;
+// }
+
+// function operate(operator, num1, num2) {
+//     let result;
+//     switch(operator) {
+//         case "+":
+//             result = add(num1, num2);
+//             break;
+//         case "-":
+//             result = substract(num1, num2);
+//             break;
+//         case "*":
+//             result = multiply(num1, num2);
+//             break;
+//         case "/":
+//             result = divide(num1, num2);
+//             break;
+//         default:
+//             result = "That's not a valid operation"
+//     }
+//     return result;
+// }
