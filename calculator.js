9ca026a105f48668e1e73ca98daa40be11c6ace9
Ejembi Emmanuel <ejembioche50@gmail.com>
2022-03-23 22:12:16 +0100

    Created operate function

--- calculator.js
+++ calculator.js
@@ -22,23 +22,23 @@ function divide (num1, num2) {
     return result;
 }
 
-// function operate(operator, num1, num2) {
-//     let result;
-//     switch(operator) {
-//         case "+":
-//             result = add(num1, num2);
-//             break;
-//         case "-":
-//             result = substract(num1, num2);
-//             break;
-//         case "*":
-//             result = multiply(num1, num2);
-//             break;
-//         case "/":
-//             result = divide(num1, num2);
-//             break;
-//         default:
-//             result = "That's not a valid operation"
-//     }
-//     return result;
-// }
+function operate(operator, num1, num2) {
+    let result;
+    switch(operator) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = substract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+        default:
+            result = "That's not a valid operation"
+    }
+    return result;
+}
