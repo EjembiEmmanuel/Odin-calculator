61ffd74e77dc00e48e4adf9939e40c4c5f146ba1
Ejembi Emmanuel <ejembioche50@gmail.com>
2022-03-24 14:57:34 +0100

    Added calculator display logic

--- calculator.js
+++ calculator.js
@@ -18,7 +18,12 @@ function multiply (num1, num2) {
 
 function divide (num1, num2) {
     let result;
-    result = num1 / num2;
+    if(num2 == 0) {
+        result = "Math Error";
+    } else {
+        result = num1 / num2;
+    }
+    
     return result;
 }
 
@@ -45,17 +50,51 @@ function operate(operator, num1, num2) {
 
 const buttons = document.querySelectorAll('button');
 const displayInput = document.querySelector('.display-input');
+const displayOutput = document.querySelector('.display-output');
 
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let input = ""
+let result = 0;
+
+displayOutput.textContent = result;
 
 function populateDisplay(e) {
     if(e.target.textContent !== "CLEAR" && e.target.textContent !== "DELETE" && e.target.textContent !== "=") {
-        console.log(e.target.textContent);
-        displayInput.textContent = e.target.textContent;
-    } else if(e.target.textContent === "CLEAR") {
-        displayInput.textContent = 0;
-    }
-    
+        if(!"+-/*".includes(e.target.textContent)) {
+            input += e.target.textContent;
+        } else {
+            input += " " + e.target.textContent + " ";
+            operator = e.target.textContent;
+        }
+    } else if("=".includes(e.target.textContent)) {
+        result = operate(operator, firstNumber, secondNumber);
+        displayOutput.textContent = result;
 
+    } else if(e.target.textContent === "DELETE") {
+        input = input.slice(0, -1);
+        displayInput.textContent = input;
+
+    } else if(e.target.textContent === "CLEAR") {
+        firstNumber = "";
+        secondNumber = "";
+        input = "";
+        result = 0;
+        displayInput.textContent = "";
+        displayOutput.textContent = result;
+    } 
+
+    displayInput.textContent = input;
+    let numbers = input.split(" ");
+
+    if(numbers[0] % 1 == 0 || numbers[2] % 1 == 0) {
+        firstNumber = parseInt(numbers[0]);
+        secondNumber = parseInt(numbers[2]);
+    } else {
+        firstNumber = parseFloat(numbers[0]);
+        secondNumber = parseFloat(numbers[2]);
+    }
 }
 
 buttons.forEach(button => button.addEventListener('click', populateDisplay));
